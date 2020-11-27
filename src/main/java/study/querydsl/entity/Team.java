95d958948b34bbb6dfcd3da4798f75d6669546e6
syleemk <syleemk@gmail.com>
2020-11-27 17:51:43 +0900

    Initial commit

---
+++ src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,32 @@
+package study.querydsl.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Team {
+
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    // new 해서 리스트 객체 넣어주는 것은 그냥 관례
+    // null 포인터 exception같은 거 피할려고 
+    // 메모리도 얼마 안먹음 어차피
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
